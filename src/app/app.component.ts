f1a4f6d43caac41d24f39882869b90a52a88ad1d
Nick Gomez <ngomez@itnes.com>
2017-06-12 22:20:18 -0600

    refactored app to use Hero class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   /*templateUrl: './app.component.html',*/
-  template: '<h1>{{title}}</h1><h2>{{hero}}</h2>',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details</h2>
+    <div><label>id:</label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+    `,
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero= 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  };
 }
