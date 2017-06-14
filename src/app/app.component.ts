7511551d4c1b6f13318ccbd8e82457264c2ccdf3
Nick Gomez <ngomez@itnes.com>
2017-06-13 20:46:37 -0600

    Added a form control to change the hero's name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,10 @@ import { Hero } from './hero';
   template: `
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details</h2>
-    <div><label>id:</label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
     `,
   styleUrls: ['./app.component.css']
 })
